7471f58944459fbe8d26000327128ab4ae6181b1
Bruno Henrique - Garu <squall.bruno@gmail.com>
2012-12-11 16:51:55 -0200

    added dependencies libmysqlclient-dev ruby-dev

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ source ~/.zshrc #reload
 
 # install dependencies
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev \
-libyaml-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 \
+libyaml-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libmysqlclient-dev ruby-dev\
 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison  \
 subversion pkgconfig libmysqlclient-dev libpq-dev bison libxml2-dev libxslt1-dev zlib1g-dev
 
