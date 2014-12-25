cb7f6a4599cc81bd60b2c001fbac656941bd3832
Bruno Henrique - Garu <squall.bruno@gmail.com>
2014-12-25 11:17:35 -0200

    Adds .profile

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 git clone git@github.com:brunohenrique/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles && git submodule update --init && cd vimfiles && git submodule update --init
 
-mkdir -p ~/.vim/
 cp -r .scripts/ ~/.scripts
+
+ln -s ~/.dotfiles/.bashrc ~/
+ln -s ~/.dotfiles/.editorconfig ~/
+ln -s ~/.dotfiles/.gemrc ~/
+ln -s ~/.dotfiles/.gitconfig ~/
+ln -s ~/.dotfiles/.gitignore ~/
+ln -s ~/.dotfiles/.irbrc ~/
+ln -s ~/.dotfiles/.profile ~/
+ln -s ~/.dotfiles/.pryrc ~/
+ln -s ~/.dotfiles/.tmux.conf ~/
+
+mkdir -p ~/.vim/
 
 ln -s ~/.dotfiles/vimfiles/.vimrc ~/
 ln -s ~/.dotfiles/vimfiles/.vimrc.after ~/
@@ -12,19 +23,11 @@ ln -s ~/.dotfiles/vimfiles/colors ~/.vim/
 
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-ln -s ~/.dotfiles/.bashrc ~/
-ln -s ~/.dotfiles/.editorconfig ~/
-ln -s ~/.dotfiles/.gemrc ~/
-ln -s ~/.dotfiles/.gitconfig ~/
-ln -s ~/.dotfiles/.gitignore ~/
-ln -s ~/.dotfiles/.irbrc ~/
-ln -s ~/.dotfiles/.pryrc ~/
-ln -s ~/.dotfiles/.tmux.conf ~/
 
 vim +BundleInstall +qall
 
 git clone git@github.com:powerline/fonts.git ~/fonts
 cd ~/fonts && ./install.sh
-
 rm -r ~/fonts
+
 source ~/.bashrc
