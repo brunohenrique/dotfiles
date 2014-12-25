b19ea9b1e9f098319f797b35d37963b809b1be65
Bruno Henrique - Garu <squall.bruno@gmail.com>
2014-12-25 11:12:39 -0200

    Adds .bashrc

--- install.sh
+++ install.sh
@@ -3,25 +3,23 @@ cd ~/.dotfiles && git submodule update --init && cd vimfiles && git submodule up
 
 mkdir -p ~/.vim/
 cp -r .scripts/ ~/.scripts
-echo "source ~/.scripts/git-completion.sh" >> ~/.bashrc
-echo "source ~/.scripts/git-prompt.sh" >> ~/.bashrc
-echo "source ~/.scripts/z.sh" >> ~/.bashrc
 
+ln -s ~/.dotfiles/vimfiles/.vimrc ~/
+ln -s ~/.dotfiles/vimfiles/.vimrc.after ~/
+ln -s ~/.dotfiles/vimfiles/.vimrc.bundles ~/
 ln -s ~/.dotfiles/vimfiles/autoload ~/.vim/
 ln -s ~/.dotfiles/vimfiles/colors ~/.vim/
-ln -s ~/.dotfiles/vimfiles/.vimrc ~/
-ln -s ~/.dotfiles/vimfiles/.vimrc.bundles ~/
-ln -s ~/.dotfiles/vimfiles/.vimrc.after ~/
 
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
+ln -s ~/.dotfiles/.bashrc ~/
 ln -s ~/.dotfiles/.editorconfig ~/
 ln -s ~/.dotfiles/.gemrc ~/
 ln -s ~/.dotfiles/.gitconfig ~/
-ln -s ~/.dotfiles/.tmux.conf ~/
 ln -s ~/.dotfiles/.gitignore ~/
 ln -s ~/.dotfiles/.irbrc ~/
 ln -s ~/.dotfiles/.pryrc ~/
+ln -s ~/.dotfiles/.tmux.conf ~/
 
 vim +BundleInstall +qall
 
