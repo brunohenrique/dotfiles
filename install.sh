a30266acffcceb3be15fab1a5f911eb703e4a2f7
Bruno Henrique - Garu <squall.bruno@gmail.com>
2016-02-15 22:18:15 -0200

    Updates instalation to use VimPlug instead NeoBundle

--- install.sh
+++ install.sh
@@ -33,15 +33,15 @@ mkdir -p ~/.vim/
 create_links vimfiles/
 
 
-if [ -s ~/.vim/bundle ]; then
-  echo "NeoBundle Already Installed"
+if [ -s ~/.vim/autoload/plug.vim ]; then
+  echo "VimPlug already installed"
 else
-  mkdir -p ~/.vim/bundle
-  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 
-vim +NeoBundleInstall +qall
+vim +PlugInstall +qall
 
 git clone git@github.com:powerline/fonts.git ~/fonts
 cd ~/fonts && ./install.sh
