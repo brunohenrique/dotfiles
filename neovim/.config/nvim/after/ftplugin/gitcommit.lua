3fe71d77182da14dad2863419f7b375af83622ac
Bruno Henrique - Garu <squall.bruno@gmail.com>
2022-03-27 20:00:00 +0200

    feat(neovim): remove abbreviations

--- neovim/.config/nvim/after/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -4,7 +4,3 @@ opt.spell     = true
 opt.textwidth = 72
 opt.colorcolumn:append { vim.opt.textwidth:get() }
 opt.colorcolumn:append { 51 }
-
-vim.cmd([[
-  iabbrev <buffer> co-author@ Co-authored-by: author <author@example.com><ESC>F:wve
-]])
--- neovim/.config/nvim/after/ftplugin/go.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd([[
-  iabbrev <buffer> debug@ println("============")<ESC>ofmt.Printf("%+v\n",DEBUG)<ESC>oprintln("============")<ESC>kFDdwi
-]])
