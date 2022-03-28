10499829dfdeb8b6ad9d6f2213d3eb784c19300e
Bruno Henrique - Garu <squall.bruno@gmail.com>
2022-03-28 19:22:21 +0200

    feat(neovim): improve lualine setup

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -80,15 +80,24 @@ return require('packer').startup({
           options = { theme = 'nord' },
           sections = {
             lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff' },
             lualine_c = {
               {
                 'filename',
                 path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
               },
             },
+            lualine_x = {
+              { 'diagnostics', symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' } },
+              'encoding',
+              'fileformat',
+              'filetype',
+            },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
           },
           tabline = {
-            lualine_a = { 'buffers' },
+            lualine_a = { { 'buffers' } },
             lualine_b = {},
             lualine_c = {},
             lualine_x = {},
