f29087005e84fd433df79f01ed8a6b18b3142a07
Bruno Henrique - Garu <squall.bruno@gmail.com>
2023-01-25 14:08:10 +0100

    fix(neovim): add missing break line to Go snippet

--- neovim/.config/nvim/luasnippets/go.lua
+++ neovim/.config/nvim/luasnippets/go.lua
@@ -1,12 +1,12 @@
 local snippets = {
   s({ trig = 'd', desc = 'Expands to my debug statement' }, fmt([[
   println("================")
-  fmt.Printf("%+v", {})
+  fmt.Printf("%+v\n", {})
   println("================")
   ]], { i(1, "DEBUG") })),
 
   s({ trig = 'fmtp', desc = 'fmt.Printf' }, fmt([[
-  fmt.Printf("%+v", {})
+  fmt.Printf("%+v\n", {})
   ]], { c(1, { i(1, "DEBUG"), i(1, "ANOTHER OPTION") }) })),
 
   s({ trig = 'fmts', desc = 'fmt.Sprintf' }, fmt([[
